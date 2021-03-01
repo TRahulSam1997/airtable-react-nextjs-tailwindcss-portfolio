1f354474d4b825d479c98cf986237b31ac0845e0
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-03-01 20:32:52 +1100

    _app.js and index.js update

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,11 @@ import '../styles/globals.css'
 import '../styles/index.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="container mx-auto my-10 max-w-xl">
+        <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
