8537f2e75fcbeb8f20f34019968a68931aa4e29c
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-03-03 21:09:57 +1100

    dataContext file updated

--- contexts/dataContext.js
+++ contexts/dataContext.js
@@ -1,4 +1,32 @@
 import {createContext, useState} from 'react';
 
-const dataContext = createContext();
+const dataItemsContext = createContext();
 
+const dataItemsProvider = ({children}) => {
+
+    const [dataItems, setDataItems] = useState([]);
+
+    const refreshDataItems = async () => {
+        try {
+            const res = await fetch('/api/getData');
+            const latestDataItems = await res.json();
+            setDataItems(latestDataItems);
+        } catch(e) {
+            console.error(e)
+        }
+    }
+
+    return (
+        <dataItemsContext.Provider
+            value={{
+                dataItems,
+                setDataItems,
+                refreshDataItems
+            }}
+        >
+            {children}
+        </dataItemsContext.Provider>
+    );
+}
+
+export {dataItemsProvider, dataItemsContext}
