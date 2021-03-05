c30980921b51644388cdffa16263162ad4ab1445
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-03-05 19:36:36 +1100

    api update

--- pages/api/utils/Airtable.js
+++ pages/api/utils/Airtable.js
@@ -7,9 +7,6 @@ const minifyRecords = (records) => {
     return records.map((record) => getMinifiedRecord(record));
 };
 const getMinifiedRecord = (record) => {
-    if (!record.fields.completed) {
-        record.fields.completed = false;
-    }
     return {
         id: record.id,
         fields: record.fields,
