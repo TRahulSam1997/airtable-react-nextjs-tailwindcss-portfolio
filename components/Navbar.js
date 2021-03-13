b96e0a533f5c56c5aa225b0545dc1d873f34e478
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-03-13 16:09:06 +1100

    Navbar updates

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
                     <div>
                         <a className="flex items-center text-gray-800 hover:text-indigo-600" href="#">
                             <svg className="h-6 w-6 sm:h-6 sm:w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
+                                <path strokeLinecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
                             </svg>
 
                             <span className="mx-3 font-medium text-sm md:text-base">{process.env.NEXT_PUBLIC_FIRST_NAME} {process.env.NEXT_PUBLIC_SECOND_NAME}</span>
